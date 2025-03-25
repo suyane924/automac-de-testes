a13bcac8a62dc0d25c9b98f74fd62e323d7b8a31
Suyane Bonfanti Dos Santos <suyane.97622@alunosatc.edu.br>
2025-03-25 19:58:41 -0300

    Primeiro commit

--- test/banco.test.js
+++ test/banco.test.js
@@ -3,5 +3,23 @@ const Banco = require("../src/banco");
 describe("Testes da classe Banco", () => {
   let conta;
 
-  test("testes", async () => {});
+  beforeEach(() => {
+    conta = new Banco('Yuri Bopre', 10);
+  });
+
+  test("deposito de 20 reais", async () => {
+    expect (conta.depositar(20));toBe(20);
+  });
+
+  test ("saque de 5 reais", async () => {
+    expect(conta.sacar(5)).toBe(5);
+  });
+
+  test ("transferencia de 18 reais", async () => {
+    conta.transferir(5, contaDestino);
+    expect(conta.obterSaldo()).toBe(5);
+    expect(contaDestino.obterSaldo()).toBe(1005)
+  });
+
 });
+
